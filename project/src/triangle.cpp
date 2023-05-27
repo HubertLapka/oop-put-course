f62275693a881e409b08e6aa78058144e1e38119
HubertLapka <126490401+HubertLapka@users.noreply.github.com>
2023-05-27 13:25:35 +0200

    Update 1 triangle.cpp

--- project/src/triangle.cpp
+++ project/src/triangle.cpp
@@ -6,7 +6,18 @@ using namespace std;
 Triangle::Triangle(float s1, float s2, float s3): side1(s1), side2(s2), side3(s3) {}
 void Triangle::show_dimensions()
 {
-    cout << "triangle with dimensions: " << side1 << "\t" << side2 << "\t" << side3 << endl;
+    float m = max(side1, max(side2, side3));
+    float s = side1+side2+side3 - m;
+    if(m >= s)
+      {
+          std::string exception = "such a triangle cannot be formed";
+          throw std::invalid_argument(exception);
+      }
+      else
+      {
+           cout << "triangle with dimensions: " << side1 << "\t" << side2 << "\t" << side3 << endl;
+      }
+
 }
 float Triangle::calculate_circumference()
 {
